faceb69a9f2b2dabde5e21efa9a4a8b6554cf169
brandonc9834 <brandonc9834@gmail.com>
2023-10-12 23:30:31 -0700

    this is done

--- .js/.js
+++ .js/.js
@@ -1,5 +1,5 @@
 function generateRandomNumber(num) {
-    return Math.floor(math.random() * num) 
+    return Math.floor(Math.random() * num) 
 }
 
     const workoutPlan = {
@@ -12,5 +12,26 @@ function generateRandomNumber(num) {
 
     for (let prop in workoutPlan) {
         let optionIdx = generateRandomNumber(workoutPlan[prop].length)
-    }
     
+
+        switch(prop) {
+            case 'day':
+                trainingDay.push(`Today's day is ${workoutPlan[prop][optionIdx]}.`)
+                break
+            case 'bodyPart': 
+                trainingDay.push(`You should do a ${workoutPlan[prop][optionIdx]} workout.`)
+                break
+            case 'cardio':
+                trainingDay.push(`we should also finish with ${workoutPlan[prop][optionIdx]}. `)
+                break
+            default: 
+                trainingDay.push('There is not enough info.')
+        }
+    }
+
+    function formatWisdom(wisdom) {
+        const formatted = trainingDay.join('\n')
+        console.log(formatted)
+      }
+      
+      formatWisdom(trainingDay);
